8b1e6f9b6054ffc845b28040efeb7895e7ca39d0
Niktoals <117472374+Niktoals@users.noreply.github.com>
2023-05-05 02:08:33 +0300

    Fixed activeting files

--- Jarvis/Jarvis.py
+++ Jarvis/Jarvis.py
@@ -3,7 +3,6 @@ import pyaudio
 import os
 import json
 import pyautogui as pg
-import keyboard
 import time
 from fuzzywuzzy import fuzz
 import random
@@ -43,47 +42,40 @@ def analis_of_digits(res):
         return 400
     if fuzz.partial_ratio("пять", res["text"])>=90:
         return 500    
+    
+def activating(res):
+    if not os.path.exists('settings'):
+        os.mkdir('settings')
+    else:
+        for filename in os.listdir('settings'):
+            if fuzz.partial_ratio(res, filename[:filename.rfind('.'):])>=70:
+                return filename
 
-def user_set_do(settings_do, res, count_scr):
-
-
+def user_set_do(res, count_scr):
     if fuzz.partial_ratio("пауза", res["text"][6::])>=90:
         pg.press('space')
         
+    if fuzz.partial_ratio('фото', res["text"])>=80:
+        play(AudioSegment.from_wav(f'C:/Users/1/Desktop/Progs/Jarvis Sound Pack от Jarvis Desktop/{random.choice(["Да сэр", "Загружаю сэр", "Есть", "Как пожелаете ", "К вашим услугам сэр", "Запрос выполнен сэр", "Образ создан"])}.wav'))
+        screen=pg.screenshot()
+        if not os.path.exists("photos"):
+            os.mkdir("photos")
+        screen.save(f"photos/screen{count_scr}.png")
+        count_scr+=1
 
-    for key in settings_do.keys():
-        id_key=res["text"].find(key[1::])
-        
-        if fuzz.partial_ratio(key[1::], res["text"])>=80 and settings_do[key]=='0':
-            play(AudioSegment.from_wav(f'C:/Users/1/Desktop/Progs/Jarvis Sound Pack от Jarvis Desktop/{random.choice(["Да сэр", "Загружаю сэр", "Есть", "Как пожелаете ", "К вашим услугам сэр", "Запрос выполнен сэр", "Образ создан"])}.wav'))
-            screen=pg.screenshot()
-            if not os.path.exists("photos"):
-                os.mkdir("photos")
-            screen.save(f"photos/screen{count_scr}.png")
-            count_scr+=1
+    if fuzz.partial_ratio('найди', res["text"])>=90:
+        play(AudioSegment.from_wav(f'C:/Users/1/Desktop/Progs/Jarvis Sound Pack от Jarvis Desktop/{random.choice(["Да сэр", "Загружаю сэр", "Есть", "Как пожелаете ", "К вашим услугам сэр", "Запрос выполнен сэр", "Образ создан"])}.wav'))
+        fPath = "C:/Program Files/Google/Chrome/Application/chrome.exe %s"
+        webbrowser.get(fPath).open("https://www.google.com/search?q=" + res["text"][res['text'].find('найди')+6::])
 
-        if fuzz.partial_ratio(key[1::], res["text"])>=90 and settings_do[key]=='0':
-            play(AudioSegment.from_wav(f'C:/Users/1/Desktop/Progs/Jarvis Sound Pack от Jarvis Desktop/{random.choice(["Да сэр", "Загружаю сэр", "Есть", "Как пожелаете ", "К вашим услугам сэр", "Запрос выполнен сэр", "Образ создан"])}.wav'))
-            fPath = "C:/Program Files/Google/Chrome/Application/chrome.exe %s"
-            webbrowser.get(fPath).open("https://www.google.com/search?q=" + res["text"][id_key+6::])
+    if fuzz.partial_ratio('запусти', res["text"])>=90:
+        filename=activating(res["text"][res['text'].find('запусти')+8::])
+        print(filename)
+        try:
+            os.startfile(f'C:/Users/1/Desktop/Progs/settings/{filename}')
+        except Exception:
+            print('Не найдено приложения в папке')
 
-        if fuzz.partial_ratio(key[1::], res["text"])>=90 and settings_do[key]!='0':
-            play(AudioSegment.from_wav(f'C:/Users/1/Desktop/Progs/Jarvis Sound Pack от Jarvis Desktop/{random.choice(["Да сэр", "Загружаю сэр", "Есть", "Как пожелаете ", "К вашим услугам сэр", "Запрос выполнен сэр", "Образ создан"])}.wav'))
-            os.startfile(settings_do[key])
-
-def reading_settings():
-    if not os.path.exists("settings.txt"):
-        f = open('settings.txt', "w", encoding='utf-8', errors='replace')
-        f.write(" играть,snake_001.py, браузер,C:/Program Files/Google/Chrome/Application/chrome.exe")
-        f.close()
-        
-    f = open('settings.txt', encoding='utf-8', errors='replace')
-    d=[n for n in f.read().split(",")]
-    settings_do={}
-    for i in range(1, len(d), 2):
-        settings_do[d[i-1]]=d[i]
-    f.close()
-    return settings_do
 
 def reading_model():
     if not os.path.exists("model.txt"):
@@ -99,25 +91,11 @@ def reading_model():
     f.close()
     return model
 
-def adding():
-    global settings_do
-    text=pg.prompt(text='"*"keyword,operation(if not 0)\n (*) 1 if screenshot; 2 if open website; else nothing', title='Input Data', default="your's input")
-    f = open('settings.txt', "a", encoding='utf-8', errors='replace')
-    if type(text)==str:
-        if len(text)!=0 and text[0] in '123456789' and text!="your's input":
-            f.write(','+text)
-        elif len(text)!=0 and text[0] not in '123456789' and text!="your's input":
-            f.write(', '+text)
-        f.close()
-        settings_do=reading_settings()
-
-
 porcupine=None
 pas_stream=None
 p=None
 
 count_scr=1
-settings_do=reading_settings()
 model = Model(reading_model())
 rec = KaldiRecognizer(model, 16000)
 
@@ -129,9 +107,6 @@ try:
     pas_stream = p.open(format=pyaudio.paInt16, channels=1, rate=porcupine.sample_rate, input=True, input_device_index=0, frames_per_buffer=porcupine.frame_length)
     while True:
         mon_bet()
-
-        if keyboard.is_pressed('ctrl') and keyboard.is_pressed('d'):
-            adding(settings_do)
 
         pcm = pas_stream.read(porcupine.frame_length)
         pcm= struct.unpack_from("h"*porcupine.frame_length, pcm)
@@ -148,13 +123,14 @@ try:
                 data = stream.read(8000)
                 if rec.AcceptWaveform(data):
                     res=json.loads(rec.FinalResult())
+                    print(res)
                     if fuzz.partial_ratio("конец", res["text"])>=70:
                         play(AudioSegment.from_wav("C:/Users/1/Desktop/Progs/Jarvis Sound Pack от Jarvis Desktop/Отключаю питание.wav"))
                         stream.close()
                         print("Вы остановили Джарвиса, жду ключевое слово")
                         break
                     if len(res["text"])!=0:
-                        user_set_do(settings_do, res, count_scr)
+                        user_set_do(res, count_scr)
                 if time.time()-start>30:
                     print("Джарвис уснул, дабы его разбудить, назовите его имя")
                     break
@@ -165,11 +141,3 @@ finally:
         pas_stream.close()
     if p is not None:
         p.terminate()
-
-    
-''' if fuzz.partial_ratio("вверх", res["text"])>=90:
-        dig = analis_of_digits(res)
-        pg.scroll(dig)
-    if fuzz.partial_ratio("вниз", res["text"])>=90:
-        dig = analis_of_digits(res)
-        pg.scroll(int(dig)*-1)'''
